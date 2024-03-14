ed4eb6345f996b4f317b341f779eadf5832e7915
PizzaLovers007 <trex@parkvue.com>
2024-03-13 21:18:45 -0700

    Save/load high score

--- Scripts/Core/GameController.gd
+++ Scripts/Core/GameController.gd
@@ -10,6 +10,7 @@ extends Node
 var has_started: bool = false
 var curr_state: GameState = GameState.START
 var _score: int = 0
+var _high_score: int = 0
 
 enum GameState {START, PLAYING, DEAD}
 
@@ -17,6 +18,7 @@ enum GameState {START, PLAYING, DEAD}
 func _ready() -> void:
 	player.player_died.connect(_on_player_died)
 	obstacle_spawner.did_tick.connect(_on_tick)
+	_load_score()
 
 
 func _on_player_died() -> void:
@@ -25,7 +27,12 @@ func _on_player_died() -> void:
 	score_text.visible = false
 	game_over_node.visible = true
 	var game_over_score_text = game_over_node.get_node("Score") as RichTextLabel
-	game_over_score_text.text = "[center]Final score: %d[/center]" % _score
+	if _score > _high_score:
+		game_over_score_text.text = "[center]Final score: %d\n[color=yellow]New high score![/color][/center]" % _score
+	else:
+		game_over_score_text.text = "[center]Final score: %d\nHigh score: %d[/center]" % [_score, _high_score]
+	_high_score = max(_high_score, _score)
+	_save_score()
 
 
 func _on_tick() -> void:
@@ -43,3 +50,18 @@ func _process(delta: float) -> void:
 				get_tree().reload_current_scene()
 			elif Input.is_action_just_pressed("ui_cancel"):
 				get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+
+func _save_score() -> void:
+	var file = FileAccess.open("user://save.json", FileAccess.WRITE)
+	file.store_32(_high_score)
+	print("saved! ", _high_score)
+
+
+func _load_score() -> void:
+	var file = FileAccess.open("user://save.json", FileAccess.READ)
+	if file == null:
+		print("no file")
+		return
+	_high_score = file.get_32()
+	print("loaded! ", _high_score)
