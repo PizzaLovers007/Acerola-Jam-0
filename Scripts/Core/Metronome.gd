97cfcdd55c12387498940b905e1443f9ae4ed7d4
PizzaLovers007 <trex@parkvue.com>
2024-03-30 17:29:59 -0700

    Mute metronome during special move phase

--- Scripts/Core/Metronome.gd
+++ Scripts/Core/Metronome.gd
@@ -1,3 +1,4 @@
+class_name Metronome
 extends Node
 
 @onready var conductor: Conductor = get_tree().get_first_node_in_group("conductor")
@@ -8,21 +9,25 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	conductor.quarter_will_pass.connect(_on_beat_passed)
 	conductor.eighth_will_pass.connect(_on_eighth_passed)
 	pass # Replace with function body.
 
 
-func _on_beat_passed(beat: int) -> void:
-	if beat % 4 == 0:
-		hi_tick_player.play()
-	else:
-		lo_tick_player.play()
-
-
 func _on_eighth_passed(beat: int, fract: int) -> void:
-	if fract != 0:
+	var measure = beat / 4;
+	var beat_norm = beat % 4
+	var half_beat_norm = beat_norm * 2 + fract
+	if measure % 4 == 3:
+		return
+	
+	if fract == 0:
+		if beat_norm == 0:
+			hi_tick_player.play()
+		else:
+			lo_tick_player.play()
+	else:
 		eighth_tick_player.play()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
